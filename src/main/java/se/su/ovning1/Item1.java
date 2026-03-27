303d385031dd8698e8f65614adb558a995ffcd65
Mika Haverås <mika.haveras@gmail.com>
2026-03-27 15:46:42 +0100

    dfdf

---
+++ src/main/java/se/su/ovning1/Item1.java
@@ -0,0 +1,12 @@
+package se.su.ovning1;
+
+public abstract class Item1 {
+    protected final String name;
+    protected Item(String name){
+        this.name = name;
+
+    }
+    public String getName() {
+        return name;
+    }
+}
