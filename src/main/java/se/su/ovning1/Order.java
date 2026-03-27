1773523ff1713f0843f9795e220bcd42fb13ffc5
Mika Haverås <mika.haveras@gmail.com>
2026-03-27 16:32:02 +0100

    Lagt till klasser

--- src/main/java/se/su/ovning1/Order.java
+++ src/main/java/se/su/ovning1/Order.java
@@ -1,4 +1,17 @@
 package se.su.ovning1;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Order {
+    private final long orderNumber;
+    private static long counter;
+    private final List<Item> items;
+    public Order(ArrayList<Item> items) {
+        this.items = items;
+    }
+    public String getReceipt() {
+        return "";
+    }
+    public double getTotalValue(){
+
+    }
 }
---
+++ src/main/java/se/su/ovning1/PriceableWithVat6.java
@@ -0,0 +1,6 @@
+package se.su.ovning1;
+
+public interface PriceableWithVat6 extends Priceable{
+    @Override
+    public double getVat();
+}
