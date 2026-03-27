42fdb391ed96f39fbe8d594908526294da80909c
Mika Haverås <mika.haveras@gmail.com>
2026-03-27 15:56:02 +0100

    Lagt till klasser

---
+++ src/main/java/se/su/ovning1/CompactDisc.java
@@ -0,0 +1,8 @@
+package se.su.ovning1;
+
+public class CompactDisc extends Recording{
+    //Denna konstruktor behöver kärlek
+    protected CompactDisc(String name) {
+        super(name);
+    }
+}
---
+++ src/main/java/se/su/ovning1/LongPlay.java
@@ -0,0 +1,9 @@
+package se.su.ovning1;
+
+public class LongPlay extends Recording{
+    protected LongPlay(String name) {
+        super(name);
+    }
+    //Denna konstruktor behöver kärlek
+
+}
