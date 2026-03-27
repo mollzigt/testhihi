1f4b0387d3b6c66089f0bccb19ff2a06b4a1d0b5
mollzigt <mollymargell@gmail.com>
2026-03-27 17:04:07 +0100

    ny upp

--- src/main/java/se/su/ovning1/Priceable.java
+++ src/main/java/se/su/ovning1/Priceable.java
@@ -5,10 +5,9 @@ public interface Priceable {
     public abstract double getPrice();
 
     public abstract double getVAT();
-    //implementera nedan
 
    default double getPriceWithVAT() {
-        double priceWithVat = this.getPrice() * this.getVAT();
+        double priceWithVat = this.getPrice() *  (1 + this.getVAT());
         return priceWithVat;
     }
 }
