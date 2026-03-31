4c0587670f551a193e7f11d4f99d9bfa4375e68b
idalenman <idapuh88@gmail.com>
2026-03-31 16:24:15 +0200

    Recording metoder

--- src/main/java/se/su/ovning1/CompactDisc.java
+++ src/main/java/se/su/ovning1/CompactDisc.java
@@ -2,15 +2,14 @@ package se.su.ovning1;
 
 public class CompactDisc extends Recording{
     //Denna konstruktor behöver kärlek
-    private String name;
-    private String artist;
-    private int year;
-    private int condition;
-    protected CompactDisc(String name, String artist, int year, int condition,
-                          double price) {
+
+    public CompactDisc(String name, String artist, int year, int condition, double price) {
         super(name, artist, year, condition, price);
 
     }
 
+    public String getType(){
+        return "CD";
+    }
 
 }
--- src/main/java/se/su/ovning1/LongPlay.java
+++ src/main/java/se/su/ovning1/LongPlay.java
@@ -1,9 +1,30 @@
 package se.su.ovning1;
 
 public class LongPlay extends Recording{
-    protected LongPlay(String name) {
-        super(name);
+
+    public LongPlay (String name, String artist, int year, int condition, double price) {
+        super(name, artist, year, condition, price);
+
     }
-    //Denna konstruktor behöver kärlek
+
+    public String getType(){
+        return "LP";
+    }
+
+    @Override
+    public double getPrice(){
+        double age = 2025-this.getYear();
+        double priceIncrease = age*5;
+        double conditionFactor= (double) this.getCondition()/10;
+        double conditionPrice = conditionFactor * this.getOriginalPrice() ;
+
+        if(age == 0 && conditionFactor == 0){
+            return getOriginalPrice();
+
+        }else{
+            return conditionPrice + priceIncrease;
+
+        }
+    }
 
 }
