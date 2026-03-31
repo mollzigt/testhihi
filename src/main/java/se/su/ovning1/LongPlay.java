2c469d8e950f61595fcece90999cfdcbc7d71889
Mika Haverås <mika.haveras@gmail.com>
2026-03-31 20:57:14 +0200

    Ändrat i getprice() så att condition kan vara 1

--- src/main/java/se/su/ovning1/LongPlay.java
+++ src/main/java/se/su/ovning1/LongPlay.java
@@ -26,7 +26,7 @@ public class LongPlay extends Recording{
     public double getPrice(){
         double price = super.getPrice()*((double) super.getCondition()/10 ) + (2025 - this.getYear()) * 5.0;
 
-        if (price < 10 ){
+        if (price < 10 && super.getCondition()!=1 ){
              return 10;
          }
         if(this.getYear()==2025){
