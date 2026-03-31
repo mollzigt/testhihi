375856bf2167f2a1af526a85809a62ac07e2afc5
Mika Haverås <mika.haveras@gmail.com>
2026-03-31 15:17:10 +0200

    hej!

--- src/main/java/se/su/ovning1/PriceableWithVAT25.java
+++ src/main/java/se/su/ovning1/PriceableWithVAT25.java
@@ -1,7 +1,7 @@
 package se.su.ovning1;
 
 public interface PriceableWithVAT25 extends Priceable {
-
+    @Override
     default double getVAT(){
         return 0.25;
     }
--- src/main/java/se/su/ovning1/PriceableWithVAT6.java
+++ src/main/java/se/su/ovning1/PriceableWithVAT6.java
@@ -2,6 +2,7 @@ package se.su.ovning1;
 
 public interface PriceableWithVAT6 extends Priceable{
 
+    @Override
     default double getVAT(){
         return 0.06;
     }
