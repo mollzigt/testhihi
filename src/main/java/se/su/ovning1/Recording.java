e64f6a0cb45ceaa95eb08f661c28391a3b5087e5
idalenman <idapuh88@gmail.com>
2026-03-31 12:43:47 +0200

    Update Recording.java

--- src/main/java/se/su/ovning1/Recording.java
+++ src/main/java/se/su/ovning1/Recording.java
@@ -1,6 +1,6 @@
 package se.su.ovning1;
 
-public abstract class Recording extends Item {
+public abstract class Recording extends Item implements PriceableWithVAT25 {
     //Denna konstruktor behöver kärlek
     protected Recording(String name) {
         super(name);
